c8234675ca3aa22400f748b7d38df2520e9c5261
krnthoang542 <81230977+simplord542@users.noreply.github.com>
2023-10-11 19:36:57 +0700

    hook

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [name, setName] = useState('mario')
+    const handleClick = ()=>{
+        setName('hoang')
+    }
+    
     return ( 
     <div className="home">
         <h2>Home page</h2>
+        <p>{name}</p>
+        <button onClick = {handleClick}>Click me</button>
+        
+        
     </div> );
 }
  
