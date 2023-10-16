de62bc2ac3157ee9b22b13ad5ee181c1cb9a79a3
krnthoang542 <81230977+simplord542@users.noreply.github.com>
2023-10-16 19:35:12 +0700

    use effect

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlog] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-]);
-    
-    return ( 
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        if(!res.ok){
+            throw Error('error')
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+        setIsPending(false);
+      })
+      .catch((err) => {
+        console.log(err.message)
+      });
+  }, []);
+
+  return (
     <div className="home">
-        <BlogList blogs = {blogs} title = "All Blogs"/>
-    </div> );
-}
- 
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+    </div>
+  );
+};
+
 export default Home;
