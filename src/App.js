fea7bbbee113562e170b3ca7670b372ed3391d0d
krnthoang542 <81230977+simplord542@users.noreply.github.com>
2023-10-17 23:31:31 +0700

    blog detail

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Create from './Create';
+import BlogDetail from './BlogDetail';
 
 function App() {
   
@@ -14,6 +15,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/create" element={<Create/>} />
+            <Route path="/blog/:id" element={<BlogDetail/>} />
           </Routes>
         </div>
       </div>
---
+++ src/BlogDetail.js
@@ -0,0 +1,20 @@
+import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
+
+const BlogDetail = () => {
+    const { id } = useParams();
+    const {data: blog, isPending} = useFetch("http://localhost:8000/blogs/" + id);
+    return ( 
+        <div className="blog-detail">
+            {isPending && <div>Loading ...</div>}
+            {blog && (
+            <article>
+                <h2>{blog.title}</h2>
+                <p>Written by {blog.author}</p>
+                <div>{blog.body}</div>
+            </article>)}
+        </div>
+     );
+}
+ 
+export default BlogDetail;
